5a82476ed6fb20d6cd43b2273dfbbef1f31e84db
CofCat456 <a25690756@gmail.com>
2024-04-27 02:46:20 +0800

    feat: update

--- test/App.test.ts
+++ test/App.test.ts
@@ -1,9 +1,7 @@
 import { describe, expect, it } from 'vitest'
 
-import { foo } from '@pkgName/index'
-
 describe('foo', () => {
   it('should exported', () => {
-    expect(foo).toBe(1)
+    expect(1).toBe(1)
   })
 })
